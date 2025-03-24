49eedfce7e5b290f23780f5a8c391c7362fd8bc4
jonas2612 <ge49zen@mytum.de>
2025-03-24 11:37:51 +0100

    add additional files from misc folder

---
+++ sbatch_utils/rastered_scripts.py
@@ -0,0 +1,38 @@
+import json
+import sys
+from pathlib import Path
+
+size = sys.argv[1]
+unit = sys.argv[2]
+overlap = sys.argv[3]
+if int(sys.argv[4])>=0:
+    intens_rat = int(sys.argv[4])
+else:
+    intens_rat = 0.1
+
+with open("/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/sample_paths.json") as f:
+    data = json.load(f)
+
+Path(f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatch_rastered_{size}{unit}").mkdir(parents=False, exist_ok=True)
+for key, value in data.items():
+    f = open(f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatch_rastered_{size}{unit}/{key}.sbatch", "w")
+    f.write(f"""#!/bin/bash
+            
+#SBATCH -p lrz-cpu
+#SBATCH --qos=cpu
+#SBATCH -t 08:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=128G
+#SBATCH -J rastered{size}_{key}
+#SBATCH -o /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/outputs/rastered{size}_{key}.out
+#SBATCH -e /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/errors/rastered{size}_{key}.err
+#SBATCH --container-image="/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sopa.sqsh"
+
+mamba activate sopa
+python /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/rastered_segmentation.py \
+ {value} \
+ /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/samples/{key}/results/Negative_Control_Rastered_{size} \
+ {size} {overlap} {unit} {intens_rat}
+            """)
+    f.close()
---
+++ sbatch_utils/voronoi_scripts.py
@@ -0,0 +1,27 @@
+import json
+from pathlib import Path
+
+with open("/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/sample_paths.json") as f:
+    data = json.load(f)
+
+Path("/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatch_voronoi").mkdir(parents=False, exist_ok=True)
+for key, value in data.items():
+    f = open(f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatch_voronoi/{key}.sbatch", "w")
+    f.write(f"""#!/bin/bash
+            
+#SBATCH --clusters=serial
+#SBATCH --partition=serial_std
+#SBATCH -t 10:00:00
+#SBATCH --mem=64G
+#SBATCH -J voronoi_{key}
+#SBATCH -o /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/outputs/voronoi_{key}.out
+#SBATCH -e /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/errors/voronoi_{key}.err
+
+source ~/.bashrc
+conda activate sopa
+mkdir -p /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/samples/{key}/results/Negative_Control_Voronoi
+python /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/voronoi_segmentation.py \
+ {value} \
+ /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/samples/{key}/results/Negative_Control_Voronoi 100000
+            """)
+    f.close()
