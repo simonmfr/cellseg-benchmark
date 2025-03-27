103489bfad76aceb2bd4de49d5dbee7d0a905cfd
jonas2612 <ge49zen@mytum.de>
2025-03-27 13:26:22 +0100

    Bugfix merscope sdata

--- scripts/merscope_sdata.py
+++ scripts/merscope_sdata.py
@@ -4,15 +4,22 @@ from subprocess import run
 
 from sopa.io.explorer import write
 from spatialdata_io import merscope
+from spatialdata import read_zarr
 
 data_path = sys.argv[1]
 save_path = sys.argv[2]
 
-sdata = merscope(data_path)
+sdata = merscope(data_path,
+                 vpt_outputs={'cell_by_gene': join(data_path, 'cell_by_gene.csv'),
+                              'cell_metadata': join(data_path, 'cell_metadata.csv'),
+                              'cell_boundaries': join(data_path, 'cell_boundaries.parquet')}
+                 )
+sdata.write(join(save_path, "sdata.zarr"))
+sdata = read_zarr(join(save_path, "sdata.zarr"))
+
 write(join(save_path, "sdata.explorer"), sdata,
         gene_column="gene",
         ram_threshold_gb=4,
         pixel_size=0.108)
-sdata.write(join(save_path, "sdata.zarr"))
 run(["rm", "-r", join(save_path, "sdata.zarr", "images")])
 run(["rm", "-r", join(save_path, "sdata.zarr", "points")])
