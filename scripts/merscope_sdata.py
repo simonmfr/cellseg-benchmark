80f68dd3e1100f49be609d7cea1c77ad157a3ca4
jonas2612 <ge49zen@mytum.de>
2025-03-28 09:37:19 +0100

    bugfix merscope
    vtp_outputs converted to path type

--- scripts/merscope_sdata.py
+++ scripts/merscope_sdata.py
@@ -1,5 +1,6 @@
 import sys
 from os.path import join
+from pathlib import Path
 from subprocess import run
 
 from sopa.io.explorer import write
@@ -10,9 +11,9 @@ data_path = sys.argv[1]
 save_path = sys.argv[2]
 
 sdata = merscope(data_path,
-                 vpt_outputs={'cell_by_gene': join(data_path, 'cell_by_gene.csv'),
-                              'cell_metadata': join(data_path, 'cell_metadata.csv'),
-                              'cell_boundaries': join(data_path, 'cell_boundaries.parquet')}
+                 vpt_outputs={'cell_by_gene': Path(join(data_path, 'cell_by_gene.csv')),
+                              'cell_metadata': Path(join(data_path, 'cell_metadata.csv')),
+                              'cell_boundaries': Path(join(data_path, 'cell_boundaries.parquet'))}
                  )
 sdata.write(join(save_path, "sdata.zarr"))
 sdata = read_zarr(join(save_path, "sdata.zarr"))
