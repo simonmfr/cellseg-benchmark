1c7d5b6c44657a88056bb0f8c50dd7050e2ad7fd
jonas2612 <ge49zen@mytum.de>
2025-04-01 15:19:19 +0200

    add exception for nuclei model

--- scripts/sbatch_utils/baysor_script_creation.py
+++ scripts/sbatch_utils/baysor_script_creation.py
@@ -15,11 +15,36 @@ Path(
     f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatch_Baysor_CP{CP_version}_{staining}"
 ).mkdir(parents=False, exist_ok=True)
 for key, value in data.items():
-    f = open(
-        f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatch_Baysor_CP{CP_version}_{staining}/{key}_{confidence}.sbatch",
-        "w",
-    )
-    f.write(f"""#!/bin/bash
+    if staining == "nuclei":
+        f = open(
+            f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatch_Baysor_CP{CP_version}_{staining}/{key}_{confidence}.sbatch",
+            "w",
+        )
+        f.write(f"""#!/bin/bash
+
+#SBATCH -p lrz-cpu
+#SBATCH --qos=cpu
+#SBATCH -t 1-00:00:00
+#SBATCH --mem=300G
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=25
+#SBATCH -J Baysor_{key}_CP1_{staining}_{confidence}
+#SBATCH -o /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/outputs/Baysor_{key}_CP1_{staining}_{confidence}.out
+#SBATCH -e /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/errors/Baysor_{key}_CP1_{staining}_{confidence}.err
+#SBATCH --container-image="/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sopa.sqsh"
+
+source ~/.bashrc
+conda activate sopa
+mkdir -p /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/samples/{key}/results/Baysor_2D_CP1_{staining}_model_{confidence}
+python /dss/dssfs03/pn52re/pn52re-dss-0001/Git/cellseg-benchmark/scripts/api_baysor.py {value} Cellpose_1_{staining}_model {confidence} {key}
+            """)
+        f.close()
+    else:
+        f = open(
+            f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatch_Baysor_CP{CP_version}_{staining}/{key}_{confidence}.sbatch",
+            "w",
+        )
+        f.write(f"""#!/bin/bash
 
 #SBATCH -p lrz-cpu
 #SBATCH --qos=cpu
@@ -37,4 +62,4 @@ conda activate sopa
 mkdir -p /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/samples/{key}/results/Baysor_2D_CP{CP_version}_DAPI_{staining}_{confidence}
 python /dss/dssfs03/pn52re/pn52re-dss-0001/Git/cellseg-benchmark/scripts/api_baysor.py {value} Cellpose_{CP_version}_DAPI_{staining} {confidence} {key}
             """)
-    f.close()
+        f.close()
