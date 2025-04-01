5fffdad099a2136d440a2b564b6d7990e6459ae2
jonas2612 <ge49zen@mytum.de>
2025-04-01 23:55:31 +0200

    reorder add ficture factor level images to utils

--- src/metrics/ficture_intensities.py
+++ src/metrics/ficture_intensities.py
@@ -73,12 +73,12 @@ def ficture_intensities(
         try:
             image_stack
         except NameError:
-            image_stack = create_factor_level_image(ficture_pixels, factor, DAPI_shape)
+            image_stack = ficture_utils.create_factor_level_image(ficture_pixels, factor, DAPI_shape)
         else:
             image_stack = np.concatenate(
                 (
                     image_stack,
-                    create_factor_level_image(ficture_pixels, factor, DAPI_shape),
+                    ficture_utils.create_factor_level_image(ficture_pixels, factor, DAPI_shape),
                 ),
                 axis=0, dtype=np.uint16,
             )
@@ -91,44 +91,3 @@ def ficture_intensities(
         columns=[f"{i}_intensity" for i in unique_factors],
     )
     return pd_intensity
-
-def create_factor_level_image(data, factor, DAPI_shape) -> np.ndarray:
-    """Compute image for given factor.
-
-    Args:
-        data: ficture data
-        factor: factor to compute image for
-        DAPI_shape: target shape
-
-    Returns: image of factor
-
-    """
-    print(f"working on factor: {factor}")
-    K1_ind = data["K1"] == factor
-    K1 = data[K1_ind]
-    K1["probability"] = K1["P1"]
-
-    K2_ind = data["K2"] == factor
-    K2 = data[K2_ind]
-    K2["probability"] = K2["P2"]
-
-    K3_ind = data["K3"] == factor
-    K3 = data[K3_ind]
-    K3["probability"] = K3["P3"]
-
-    filtered_data = pd.concat([K1, K2, K3], axis=0)[
-        ["Y_pixel", "X_pixel", "probability"]
-    ]
-    del K1, K2, K3
-
-    bins_y = np.linspace(0, DAPI_shape[1], num=DAPI_shape[1] + 1)
-    bins_x = np.linspace(0, DAPI_shape[0], num=DAPI_shape[0] + 1)
-    image, _, _ = np.histogram2d(
-        filtered_data["Y_pixel"],
-        filtered_data["X_pixel"],
-        bins=[bins_x, bins_y],
-        weights=filtered_data["probability"],
-    )
-    image = np.clip(np.around(image * 65535), 0, 65535).astype(np.uint16)
-    image = image[np.newaxis, :]
-    return image
