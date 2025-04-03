ad287965241211c037b347a0740c945a1f97c5e3
jonas2612 <ge49zen@mytum.de>
2025-04-03 18:39:56 +0200

    Make instance key uniform for uniformity

--- src/sdata_utils.py
+++ src/sdata_utils.py
@@ -275,6 +275,9 @@ def transform_adata(sdata_main: sd.SpatialData, seg_method: str, data_path):
     adata.uns['spatialdata_attrs']['region_key'] = "region"
     adata.obs['region'] = f"boundaries_{seg_method}"
     adata.obs['region'] = pd.Categorical(adata.obs['region'])
+    adata.obs['cell_id'] = adata.obs[adata.uns['spatialdata_attrs']['instance_key']]
+    adata.obs['cell_id'] = pd.Categorical(adata.obs['cell_id'])
+    adata.uns['spatialdata_attrs']['instance_key'] = "cell_id"
 
     sdata_main[f"adata_{seg_method}"] = adata
     return
