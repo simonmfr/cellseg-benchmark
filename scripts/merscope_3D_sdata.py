37be6721f63dfde4793ebe9162c929efd0690d42
jonas2612 <ge49zen@mytum.de>
2025-06-30 15:11:04 +0200

    ensure 3D boundaries

--- scripts/merscope_3D_sdata.py
+++ scripts/merscope_3D_sdata.py
@@ -3,6 +3,8 @@ from os.path import join
 from os import listdir
 from pathlib import Path
 from spatialdata_io import merscope
+from spatialdata.models import ShapesModel
+from geopandas import read_parquet
 
 data_path = sys.argv[1]
 save_path = sys.argv[2]
@@ -12,6 +14,7 @@ sdata = merscope(
     data_path,
     transcripts=False,
     mosaic_images=False,
+    cells_boundaries=False,
     vpt_outputs={
         "cell_by_gene": Path(join(save_path, "analysis_outputs", "cell_by_gene.csv")),
         "cell_metadata": Path(join(save_path, "analysis_outputs", "cell_metadata.csv")),
@@ -19,5 +22,9 @@ sdata = merscope(
     },
     z_layers=[0,1,2,3,4,5,6]
 )
+sdata['boundaries_vpt_3D'] = ShapesModel.parse(read_parquet(join(save_path, "analysis_outputs", "cellpose2_micron_space.parquet"),
+                                          columns=("ID", "EntityID", "ZIndex", "ZLevel", "Geometry")
+                                          )
+                                               )
 
 sdata.write(join(save_path, "sdata.zarr"), overwrite=True)
