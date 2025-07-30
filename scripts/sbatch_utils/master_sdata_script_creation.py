b39ab1a6c35018dd7f6375403fe94d3cfb57f959
jonas2612 <ge49zen@mytum.de>
2025-07-30 18:48:06 +0200

    Adapt requirements

--- scripts/sbatch_utils/master_sdata_script_creation.py
+++ scripts/sbatch_utils/master_sdata_script_creation.py
@@ -18,8 +18,8 @@ for key, value in data.items():
 
 #SBATCH -p lrz-cpu
 #SBATCH --qos=cpu
-#SBATCH -t 14:00:00
-#SBATCH --mem=400G
+#SBATCH -t 09:00:00
+#SBATCH --mem=150G
 #SBATCH -J master_sdata_{key}
 #SBATCH -o /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/outputs/master_sdata_{key}.out
 #SBATCH -e /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/errors/master_sdata_{key}.err
