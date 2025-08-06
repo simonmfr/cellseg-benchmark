4d388731b346188981b4d095f4c33323d4d9dc78
jonas2612 <ge49zen@mytum.de>
2025-08-06 16:20:04 +0200

    Create watershed merlin scripts

---
+++ scripts/sbatch_utils/merscope_watershed_script_creation.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+data = {}
+data['foxf2_s1_r0'] = "/dss/dssfs03/pn52re/pn52re-dss-0000/202402-Foxf2/merfish_output/watershed/20240229_mousebrain-Slide01-ws-WT-ECKO/region_0-ECKO000"
+data['foxf2_s1_r1'] = "/dss/dssfs03/pn52re/pn52re-dss-0000/202402-Foxf2/merfish_output/watershed/20240229_mousebrain-Slide01-ws-WT-ECKO/region_1-WT000"
+data['foxf2_s4_r0'] = "/dss/dssfs03/pn52re/pn52re-dss-0000/202402-Foxf2/merfish_output/watershed/20240304_mousebrain-Slide04-ws-ECKO-PCKO/region_0-PCKO421"
+data['foxf2_s4_r1'] = "/dss/dssfs03/pn52re/pn52re-dss-0000/202402-Foxf2/merfish_output/watershed/20240304_mousebrain-Slide04-ws-ECKO-PCKO/region_1-ECKO557"
+
+Path(
+    "/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatches/sbatch_merscope_watershed"
+).mkdir(parents=False, exist_ok=True)
+for key, value in data.items():
+    f = open(
+        f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatches/sbatch_merscope/{key}.sbatch",
+        "w",
+    )
+    f.write(f"""#!/bin/bash
+
+#SBATCH -p lrz-cpu
+#SBATCH --qos=cpu
+#SBATCH -t 12:00:00
+#SBATCH --mem=300G
+#SBATCH -J merscope_watershed_{key}
+#SBATCH -o /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/outputs/merscope_{key}.out
+#SBATCH -e /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/errors/merscope_{key}.err
+#SBATCH --container-image="/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sopa.sqsh"
+
+mamba activate sopa
+mkdir -p /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/samples/{key}/results/Watershed_Merlin
+python /dss/dssfs03/pn52re/pn52re-dss-0001/Git/cellseg-benchmark/scripts/merscope_sdata.py {value} \
+ /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/samples/{key}/results/Watershed_Merlin
+            """)
+    f.close()
