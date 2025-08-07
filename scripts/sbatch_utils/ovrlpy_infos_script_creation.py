45fe5b98e02ceb81430dc7eae10b633a31923b9b
jonas2612 <ge49zen@mytum.de>
2025-08-07 09:54:37 +0200

    update to ovrlpy

--- scripts/sbatch_utils/ovrlpy_infos_script_creation.py
+++ scripts/sbatch_utils/ovrlpy_infos_script_creation.py
@@ -3,7 +3,7 @@ import json
 from pathlib import Path
 
 parser = argparse.ArgumentParser(
-    description="scripts for computing ficture statistics."
+    description="scripts for computing ovrlpy statistics."
 )
 parser.add_argument(
     "--recompute", action="store_true", help="Consider genotype differentiation"
@@ -16,28 +16,27 @@ with open(
     data = json.load(f)
 
 Path(
-    "/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatches/sbatch_ficture_stats"
+    "/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatches/sbatch_ovrlpy_stats"
 ).mkdir(parents=False, exist_ok=True)
 for key, value in data.items():
     f = open(
-        f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatches/sbatch_ficture_stats/{key}.sbatch",
+        f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatches/sbatch_ovrlpy_stats/{key}.sbatch",
         "w",
     )
     f.write(f"""#!/bin/bash
 
-#SBATCH -p lrz-hgx-a100-80x4
-#SBATCH --exclude=lrz-hgx-a100-002
-#SBATCH -t 09:00:00
-#SBATCH --mem=900G
-#SBATCH --gres=gpu:1
-#SBATCH -J ficture_stats_{key}
-#SBATCH -o /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/outputs/ficture_stats_{key}.out
-#SBATCH -e /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/errors/ficture_stats_{key}.err
+#SBATCH -p lrz-cpu
+#SBATCH -q cpu
+#SBATCH -t 01:30:00
+#SBATCH --mem=50G
+#SBATCH -J ovrlpy_stats_{key}
+#SBATCH -o /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/outputs/ovrlpy_stats_{key}.out
+#SBATCH -e /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/errors/ovrlpy_stats_{key}.err
 #SBATCH --container-image="/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/cellseg_benchmark_2.sqsh"
 
 cd ~/gitrepos/cellseg-benchmark
 git pull
 mamba activate cellseg_benchmark
-python scripts/ficture_infos.py {key} {value} {"--recompute" if args.recompute else ""}
+python scripts/ovrlpy_infos.py {key} {value} {"--recompute" if args.recompute else ""}
 """)
     f.close()
