3f0f49ce7635225f96100192e777a717e655674b
jonas2612 <ge49zen@mytum.de>
2025-08-18 16:30:31 +0200

    ensure region column is cat

--- cellseg_benchmark/sdata_utils.py
+++ cellseg_benchmark/sdata_utils.py
@@ -297,6 +297,8 @@ def integrate_segmentation_data(
                 sdata_main[f"adata_{seg_method}"].obs["sample"] = (
                     cohort + "_" + slide + "_" + region
                 )
+                region_key = sdata_main[f"adata_{seg_method}"].uns['spatialdata_attrs']['region_key']
+                sdata_main[f"adata_{seg_method}"].obs[region_key] = sdata_main[f"adata_{seg_method}"].obs[region_key].astype("category")
                 if write_to_disk:
                     sdata_main.write_element(f"adata_{seg_method}")
             elif len(sdata.tables) > 1:
