985ddb4f2e9f2c7f4c3416a4dff3063f49e4e92c
jonas2612 <ge49zen@mytum.de>
2025-10-07 14:42:02 +0200

    bugfix

--- scripts/sbatch_utils/rastered_scripts.py
+++ scripts/sbatch_utils/rastered_scripts.py
@@ -20,11 +20,11 @@ with open(
     data = yaml.safe_load(f)
 
 Path(
-    f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatches/sbatch_rastered_{args.size}{args.unit}"
+    f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatches/sbatch_rastered_{args.width}{args.unit}"
 ).mkdir(parents=False, exist_ok=True)
 for key, value in data.items():
     f = open(
-        f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatches/sbatch_rastered_{args.size}{args.unit}/{key}.sbatch",
+        f"/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sbatches/sbatch_rastered_{args.width}{args.unit}/{key}.sbatch",
         "w",
     )
     f.write(f"""#!/bin/bash
@@ -33,15 +33,15 @@ for key, value in data.items():
 #SBATCH --qos=cpu
 #SBATCH -t 08:00:00
 #SBATCH --mem=128G
-#SBATCH -J rastered{args.size}_{key}
-#SBATCH -o /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/outputs/rastered{args.size}_{key}.out
-#SBATCH -e /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/errors/rastered{args.size}_{key}.err
+#SBATCH -J rastered{args.width}_{key}
+#SBATCH -o /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/outputs/rastered{args.width}_{key}.out
+#SBATCH -e /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/errors/rastered{args.width}_{key}.err
 #SBATCH --container-image="/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/sopa.sqsh"
 
 mamba activate sopa
 python /dss/dssfs03/pn52re/pn52re-dss-0001/Git/cellseg-benchmark/scripts/rastered_segmentation.py \
  {value["path"]} \
- /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/samples/{key}/results/Negative_Control_Rastered_{args.size} \
- {args.size} {args.overlap} {args.unit} {args.intens_rat}
+ /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/samples/{key}/results/Negative_Control_Rastered_{args.width} \
+ {args.width} {args.overlap} {args.unit} {args.intens_rat}
 """)
     f.close()
