a8f02752eeb51c9f38e3f802ef55cadb8a703808
jonas2612 <ge49zen@mytum.de>
2025-12-23 11:32:54 +0100

    Fix scripts cellpose models

--- scripts/sbatch_utils/cellpose2_script_creation.py
+++ scripts/sbatch_utils/cellpose2_script_creation.py
@@ -31,11 +31,11 @@ for key, value in data.items():
 #SBATCH -J CP2_{key}_{args.staining}
 #SBATCH -o /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/outputs/CP2_{key}_{args.staining}.out
 #SBATCH -e /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/logs/errors/CP2_{key}_{args.staining}.err
-#SBATCH --container-image="/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/enroot_images/sopa.sqsh"
+#SBATCH --container-image="/dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/misc/enroot_images/sopa_ABC.sqsh"
 
 mamba activate sopa
 mkdir -p /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/samples/{key}/results/Cellpose_2_DAPI_{args.staining}
-python scripts/segmentation/cellpose_2.py {value["path"]} \
+python /dss/dssfs03/pn52re/pn52re-dss-0001/Git/cellseg-benchmark/scripts/segmentation/cellpose_2.py {value["path"]} \
  /dss/dssfs03/pn52re/pn52re-dss-0001/cellseg-benchmark/samples/{key}/results/Cellpose_2_DAPI_{args.staining} {args.staining}
 """)
     f.close()
