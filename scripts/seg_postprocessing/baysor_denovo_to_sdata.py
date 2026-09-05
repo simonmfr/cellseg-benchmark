27ba93a65eb6d233ed5e9e049208d3186a7784e4
simonmfr <70199914+simonmfr@users.noreply.github.com>
2026-09-05 14:39:06 +0200

    Support 2D Baysor de novo conversion and Explorer export

--- scripts/seg_postprocessing/baysor_denovo_to_sdata.py
+++ scripts/seg_postprocessing/baysor_denovo_to_sdata.py
@@ -2,6 +2,7 @@
 import argparse
 import logging
 import pathlib
+import subprocess
 
 import anndata
 import geopandas
@@ -9,7 +10,9 @@ import h5py
 import pandas as pd
 import scipy.sparse
 import sopa.aggregation
+import sopa.io.explorer
 import sopa.utils
+import spatialdata
 import spatialdata.models
 import spatialdata_io
 
@@ -47,30 +50,44 @@ def main():
     )
     parser.add_argument("data_path", help="Path to merfish output folder.")
     parser.add_argument("save_path", help="Path to Baysor_*_denovo results folder.")
+    parser.add_argument(
+        "--explorer", action="store_true", help="Write explorer files."
+    )
     args = parser.parse_args()
 
     save_path = pathlib.Path(args.save_path)
+    data_path = pathlib.Path(args.data_path)
     baysor_out = save_path / "baysor_out"
     assert (baysor_out / "feature_matrix.h5").exists(), "not correctly computed"
 
     logger.info("Loading images...")
     sdata = spatialdata_io.merscope(
-        args.data_path,
+        data_path,
         transcripts=False,
         mosaic_images=True,
         cells_boundaries=False,
     )
 
     logger.info("Loading boundaries...")
-    boundaries = read_boundaries(baysor_out / "cell_boundaries_3d.parquet")
-    # layer is the z position in microns
-    boundaries["ZLevel"] = boundaries["layer"].astype(float)
-    boundaries["ZIndex"] = boundaries["ZLevel"].rank(method="dense").astype(int) - 1
+    boundaries_3d = baysor_out / "cell_boundaries_3d.parquet"
+    is_3d = boundaries_3d.exists()
+    if is_3d:
+        boundaries = read_boundaries(boundaries_3d)
+        boundaries["ZLevel"] = boundaries["layer"].astype(float)
+        boundaries["ZIndex"] = (
+            boundaries["ZLevel"].rank(method="dense").astype(int) - 1
+        )
+        # Estimated from all of a cell's molecules, wider than the union of z layers.
+        outlines = read_boundaries(baysor_out / "cell_boundaries.parquet")
+        intensity_shapes_key = "baysor_outlines"
+    else:
+        boundaries = read_boundaries(baysor_out / "cell_boundaries.parquet")
+        outlines = None
+        intensity_shapes_key = "baysor_boundaries"
 
-    # estimated from all of a cell's molecules, wider than the union of z layers
-    outlines = read_boundaries(baysor_out / "cell_boundaries.parquet")
     sdata["baysor_boundaries"] = spatialdata.models.ShapesModel.parse(boundaries)
-    sdata["baysor_outlines"] = spatialdata.models.ShapesModel.parse(outlines)
+    if outlines is not None:
+        sdata["baysor_outlines"] = spatialdata.models.ShapesModel.parse(outlines)
 
     logger.info("Loading counts...")
     with h5py.File(baysor_out / "feature_matrix.h5", "r") as f:
@@ -85,7 +102,8 @@ def main():
     obs["cell_id"] = obs.index.astype(str)
     obs.index = obs.index.rename(None)
 
-    adata = anndata.AnnData(counts.T.tocsr(), obs=obs, var=pd.DataFrame(index=genes))
+    var = pd.DataFrame({"gene": genes}, index=genes)
+    adata = anndata.AnnData(counts.T.tocsr(), obs=obs, var=var)
     adata = adata[adata.obs["cell_id"].isin(boundaries["cell_id"])].copy()
     adata.obs["region"] = pd.Categorical(["baysor_boundaries"] * adata.n_obs)
     sdata["table"] = spatialdata.models.TableModel.parse(
@@ -98,21 +116,42 @@ def main():
     # per z plane intensities come from intensities_3D.py, run after this
     logger.info("Aggregating channel intensities...")
     sdata["table"].obsm["intensities"] = pd.DataFrame(
-        sopa.aggregation.aggregate_channels(sdata, shapes_key="baysor_outlines"),
+        sopa.aggregation.aggregate_channels(sdata, shapes_key=intensity_shapes_key),
         columns=sopa.utils.validated_channel_names(
             sopa.utils.get_spatial_image(
                 sdata, list(sdata.images.keys())[0], return_key=True
             )[1]
         ),
-        index=sdata["baysor_outlines"].index.astype(str),
+        index=sdata[intensity_shapes_key].index.astype(str),
     ).reindex(sdata["table"].obs["cell_id"])
 
-    for i in list(sdata.images.keys()):
-        del sdata[i]
-    del sdata["baysor_outlines"]
+    if outlines is not None:
+        del sdata["baysor_outlines"]
+    if args.explorer and is_3d:
+        logger.warning("Explorer output is only supported for 2D Baysor; skipping.")
+    if not args.explorer or is_3d:
+        for i in list(sdata.images.keys()):
+            del sdata[i]
 
     logger.info("Saving data...")
-    sdata.write(str(save_path / "sdata.zarr"), overwrite=True)
+    zarr_path = save_path / "sdata.zarr"
+    sdata.write(str(zarr_path), overwrite=True)
+
+    if args.explorer and not is_3d:
+        sdata = spatialdata.read_zarr(str(zarr_path))
+        translation = pd.read_csv(
+            data_path / "images" / "micron_to_mosaic_pixel_transform.csv",
+            sep=" ",
+            header=None,
+        )
+        sopa.io.explorer.write(
+            str(save_path / "sdata.explorer"),
+            sdata,
+            gene_column="gene",
+            ram_threshold_gb=4,
+            pixel_size=1 / translation.iloc[0, 0],
+        )
+        subprocess.run(["rm", "-r", str(zarr_path / "images")])
     logger.info("Done.")
 
 
