213c88bf268f72dd46ec8c9f7e4efe795d2b29ac
jonas2612 <ge49zen@mytum.de>
2026-09-23 11:13:45 +0200

    correct function use

--- cellseg_benchmark/metrics/ficture.py
+++ cellseg_benchmark/metrics/ficture.py
@@ -17,7 +17,7 @@ from .. import _constants
 from .. import ficture_utils as fu
 from .. import sdata_utils as su
 from . import f1_score
-from .utils import clean_method_name
+from . import clean_method_name
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s")
 logger = logging.getLogger("ficture_f1")
@@ -302,7 +302,7 @@ def plot_ficture_f1(
     order = (
         mean_results.groupby("method")["f1"].mean().sort_values().index
     )
-    pal = {clean_method_name[ct]: _constants.method_colors[ct] for ct in order}
+    pal = {clean_method_name(ct): _constants.method_colors[ct] for ct in order}
     fig = plt.figure(figsize=(10, 5))
     ax = sns.boxplot(
         data=mean_results,
@@ -337,7 +337,7 @@ def plot_ficture_f1(
     order = (
         mean_results.groupby("method")["f1"].mean().sort_values().index
     )
-    pal = {clean_method_name[ct]: _constants.method_colors[ct] for ct in order}
+    pal = {clean_method_name(ct): _constants.method_colors[ct] for ct in order}
     fig = plt.figure(figsize=(10, 5))
     ax = sns.boxplot(
         data=mean_results,
